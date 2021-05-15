0733321d167f9458b9fb59aa486cfce69c5a79f7
ramanujdas <ramanujds9@gmail.com>
2021-05-15 17:40:16 +0530

    Updated Session Policy

--- spring-boot-workspace/rest-api-app/src/main/java/com/cts/example/SecurityConfig.java
+++ spring-boot-workspace/rest-api-app/src/main/java/com/cts/example/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -53,7 +54,11 @@ private UserDetailsService userDetailsService;
 			.antMatchers("/user/**").hasAnyRole("ADMIN","USER")
 			.antMatchers("/public/**").permitAll()
 			.anyRequest()
-			.authenticated();
+			.authenticated()
+			.and().
+			exceptionHandling().and().sessionManagement()
+	.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			;
 			
 			http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 		
