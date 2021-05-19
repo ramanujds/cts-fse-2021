b7520175cc28fe7126a20624ff1341dd3040bad6
ramanujdas <ramanujds9@gmail.com>
2021-05-19 19:22:46 +0530

    RestTemplate Implementation

---
+++ spring-boot-workspace/simple-rest-api-app/src/main/java/com/cts/example/SimpleRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.cts.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApiAppApplication.class, args);
+	}
+
+}
