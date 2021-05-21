6114324f6cfcc4340479ce3b9af35a0cb89b378e
ramanujdas <ramanujdas@localhost>
2021-05-21 12:59:26 +0530

    EB+RDS

---
+++ spring-boot-workspace/rest-api-rds/src/main/java/com/cts/rest/api/rds/RestApiRdsApplication.java
@@ -0,0 +1,13 @@
+package com.cts.rest.api.rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiRdsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-workspace/rest-api-rds/src/test/java/com/cts/rest/api/rds/RestApiRdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.rest.api.rds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
