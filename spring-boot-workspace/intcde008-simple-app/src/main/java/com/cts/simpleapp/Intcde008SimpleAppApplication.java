44023ad9aed5376124b46e72e6c87c63371e9f33
ramanujdas <ramanujdas@localhost>
2021-05-21 19:10:31 +0530

    Code Pipeline

---
+++ spring-boot-workspace/intcde008-simple-app/src/main/java/com/cts/simpleapp/Intcde008SimpleAppApplication.java
@@ -0,0 +1,13 @@
+package com.cts.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intcde008SimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Intcde008SimpleAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-workspace/intcde008-simple-app/src/test/java/com/cts/simpleapp/Intcde008SimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.simpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Intcde008SimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
