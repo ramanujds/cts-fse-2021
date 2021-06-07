e5330dbcf24d453af578887c3b3c05c8713972e2
ramanujdas <ramanujdas@localhost>
2021-06-07 14:02:03 +0530

    Updated Repository

--- spring-boot-workspace/aws-dynamodb-app/src/main/java/com/cts/microservices/controller/CoderController.java
+++ spring-boot-workspace/aws-dynamodb-app/src/main/java/com/cts/microservices/controller/CoderController.java
@@ -1,8 +1,12 @@
 package com.cts.microservices.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,6 +51,12 @@ public class CoderController {
 	public Coder saveCoder(@RequestBody Coder coder) {
 		log.info("Post Mapping for Saving Coder");
 		return repo.save(coder);
+	}
+	
+	@GetMapping
+	public List<Coder> getAllCoders(){
+		List<Coder> coders=(List<Coder>) repo.findAll();
+		return coders;
 	}
 	
 	
--- spring-boot-workspace/aws-dynamodb-app/src/main/java/com/cts/microservices/repository/CoderRepo.java
+++ spring-boot-workspace/aws-dynamodb-app/src/main/java/com/cts/microservices/repository/CoderRepo.java
@@ -2,11 +2,12 @@ package com.cts.microservices.repository;
 
 import java.util.Optional;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import com.cts.microservices.model.Coder;
 
-
+@EnableScan
 public interface CoderRepo extends CrudRepository<Coder, Integer>{
 
 	Optional<Coder> findById(int id);
